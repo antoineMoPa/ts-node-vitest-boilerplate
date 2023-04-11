24e8179698f62e35d230fcf114c8a62f955003aa
Antoine M-P <antoine@lumen5.com>
2023-04-11 16:10:40 -0400

    Remove errors from 'npm run build'

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'main.ts'),
+            name: 'main.ts',
+            fileName: (format) => `main.${format}.js`
+        },
+    },
+    optimizeDeps: {
+        disabled: true,
+    },
     plugins: [],
 });
